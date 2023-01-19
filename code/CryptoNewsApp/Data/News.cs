e26c9f77f1dffbeab930c49e57e99d286cb1677c
Dominic Frei <dominic.frei@mongodb.com>
2023-01-19 15:42:48 +0000

    Add images

---
+++ code/CryptoNewsApp/Data/News.cs
@@ -0,0 +1,13 @@
+﻿using System.Runtime.InteropServices.JavaScript;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace CryptoNewsApp.Data
+{
+    public class News
+    {
+        [BsonId] public ObjectId Id { get; set; }
+        [BsonElement("title")] public String Title { get; set; }
+        [BsonElement("date")] public DateTime Date { get; set; }
+    }
+}
